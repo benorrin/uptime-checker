50d9f48d0c780de3c9bc69528a85ce1eb4e27cc6
Ben Orrin <114030615+benorrin@users.noreply.github.com>
2023-11-10 16:51:31 +0000

    add PING_INTERVAL_SECONDS constant

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 use std::error::Error;
 use std::fs::OpenOptions;
+use std::io::{self, Write};
 use std::thread;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
+use reqwest;
 use csv::Writer;
 
 const URL_TO_CHECK: &str = "https://example.com";
 const CSV_FILE_PATH: &str = "log.csv";
+const PING_INTERVAL_SECONDS: u64 = 900; // Change this value to set the interval in seconds
 
 async fn check_url_and_log() -> Result<(), Box<dyn Error>> {
     match reqwest::get(URL_TO_CHECK).await {
         Ok(response) => {
             let status_code = response.status();
-            let timestamp = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
+            let timestamp = SystemTime::now()
+                .duration_since(UNIX_EPOCH)?
+                .as_secs();
 
             println!(
                 "Timestamp: {}, URL: {}, Status Code: {} (Accessible)",
@@ -30,7 +35,9 @@ async fn check_url_and_log() -> Result<(), Box<dyn Error>> {
             csv_writer.write_record(&[timestamp.to_string(), status_code.to_string()])?;
         }
         Err(err) => {
-            let timestamp = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
+            let timestamp = SystemTime::now()
+                .duration_since(UNIX_EPOCH)?
+                .as_secs();
 
             eprintln!(
                 "Timestamp: {}, URL: {}, Error: {} (Not Accessible)",
@@ -43,17 +50,19 @@ async fn check_url_and_log() -> Result<(), Box<dyn Error>> {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ping_interval = Duration::from_secs(PING_INTERVAL_SECONDS);
+
     loop {
         let current_time = SystemTime::now();
         let elapsed_time = match current_time.duration_since(UNIX_EPOCH) {
-            Ok(duration) => duration.as_secs() % 900,
+            Ok(duration) => duration.as_secs() % PING_INTERVAL_SECONDS,
             Err(err) => {
                 eprintln!("Error getting duration since UNIX_EPOCH: {:?}", err);
                 continue;
             }
         };
 
-        let next_run_time = current_time + Duration::from_secs(900 - elapsed_time);
+        let next_run_time = current_time + Duration::from_secs(PING_INTERVAL_SECONDS - elapsed_time);
         let sleep_duration = match next_run_time.duration_since(current_time) {
             Ok(duration) => duration,
             Err(err) => {
@@ -73,4 +82,3 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 }
-
